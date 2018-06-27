2f7ebeaef89f21544c5dc0d6088e4bbdf8062299
princeallan <allankirui57@gmail.com>
2018-06-27 19:33:21 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,36 @@
+//Boostrapping
+var app = angular.module('firstApp',  ['ngRoute']);
+
+//Templating and routes
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+
+    when('/addEmployee', {
+        templateUrl: 'addEmployee.html',
+        controller: 'AddEmployeeController'
+    }).
+
+    when('/viewEmployees', {
+        templateUrl: 'viewEmployees.html',
+        controller: 'ViewEmployeeController'
+    }).
+
+    otherwise({
+        redirectTo: '/addEmployee'
+    });
+}]);
+
+//controllers and scope (show better use of scope)
+app.controller('appController', function($scope) {
+    $scope.name = "eg Mondo Ride";
+});
+
+app.controller('AddEmployeeController', function($scope) {
+    $scope.message = "This page will be used to display add student form";
+});
+
+app.controller('ViewEmployeeController', function($scope) {
+    $scope.message = "This page will be used to display all the students";
+});
+
+//Custom filters
